6014469af4aa694ed792713c3097ff3a1e546a62
Emircan EGE <ege61emir@gmail.com>
2024-11-19 14:42:36 +0300

    Add task controller.

--- TaskManagementApi/Controllers/TaskController.cs
+++ TaskManagementApi/Controllers/TaskController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TaskManagementApi.Core.Model;
 using TaskManagementApi.Data;
 
 namespace TaskManagementApi.Controllers
@@ -14,5 +15,42 @@ namespace TaskManagementApi.Controllers
         {
             _context = context;
         }
+
+        [HttpGet]
+        public IActionResult GetTasks() {
+            return Ok(_context.Task.ToList());
+        }
+
+        [HttpPost]
+        public IActionResult CreateTask(TaskItem task)
+        {
+            _context.Task.Add(task);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetTasks), new { id = task.Id }, task);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateTask(int id, TaskItem updatedTask)
+        {
+            var task = _context.Task.Find(id);
+            if (task == null) return NotFound();
+
+            task.Title = updatedTask.Title;
+            task.Description = updatedTask.Description;
+            task.IsCompleted = updatedTask.IsCompleted;
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteTask(int id)
+        {
+            var task = _context.Task.Find(id);
+            if (task == null) return NotFound();
+
+            _context.Task.Remove(task);
+            _context.SaveChanges();
+            return NoContent();
+        }
     }
-}
+} 
